8e6c5930e6ce1c64cce31f547e0c75706a803d65
vinusuhas-codecraft <vinu.dlima@codecraft.co.in>
2023-08-10 14:30:29 +0530

    feat: implemtnted calc.js functions

---
+++ calc.js
@@ -0,0 +1,17 @@
+
+    function appendToDisplay(value) {
+      document.getElementById('display').value += value;
+    }
+
+    function calculate() {
+      let expression = document.getElementById('display').value;
+      try {
+        document.getElementById('display').value = eval(expression);
+      } catch (error) {
+        document.getElementById('display').value = 'Error';
+      }
+    }
+
+    function clearDisplay() {
+      document.getElementById('display').value = '';
+    }
